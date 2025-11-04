0e9b44531b227491f3392dd284746623f74e4329
LuizDev544 <luizfernando.carrupt@gmail.com>
2025-11-04 16:19:10 -0300

    Começando

---
+++ demo/src/main/java/com/example/demo/Projectm2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projectm2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projectm2Application.class, args);
+	}
+
+}
