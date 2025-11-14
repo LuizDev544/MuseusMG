6c1829945d93a69c8a6c4b9c3e9eeffd8a8273ed
joaoppppppppppppp <luizfernando.carrupt@gmail.com>
2025-11-14 10:51:32 -0300

    Update Cadastro.js

--- FrontEnd/scripts/Cadastro.js
+++ FrontEnd/scripts/Cadastro.js
@@ -13,17 +13,30 @@ form.addEventListener("submit", async (e) => {
         duracaoDoEvento: document.getElementById("duracaoDoEvento").value,
         preco: document.getElementById("preco").value
     };
-    
-    try {
-        
-    const data = await response.json();
-    console.log("Evento cadastrado:", data);
 
-    msg.textContent = "Evento cadastrado com sucesso!";
-    msg.style.color = "green";
-    form.reset();
-} catch (err) {
-    console.error("Erro ao cadastrar evento:", err);
-    msg.textContent = "Erro: " + err.message;
-    msg.style.color = "red";}
-})  
+    try {
+        const response = await fetch("http//localhost:8080/api/admin/museus", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(evento)
+        });
+
+        if (!response.ok) {
+            throw new Error("Erro na requisição: " + response.status);
+        }
+
+        const data = await response.json();
+        console.log("Evento cadastrado:", data);
+
+        msg.textContent = "Evento cadastrado com sucesso!";
+        msg.style.color = "green";
+        form.reset();
+
+    } catch (err) {
+        console.error("Erro ao cadastrar evento:", err);
+        msg.textContent = "Erro: " + err.message;
+        msg.style.color = "red";
+    }
+});
